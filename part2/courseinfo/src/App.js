78ddb8cd4c50252c0815fb35653d79326d19c3f7
strebor19 <arobertamaro@gmail.com>
2023-07-21 15:34:27 -0400

    do 2.1 courseinfo, step 6

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,49 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  }
+
+  return <Course course={course}/>
+}
+
+const Course = ({course}) => {
+  return <div>
+    <Header name={course.name}/>
+    <Content parts={course.parts}/>
+  </div> 
+}
+
+const Header = ({name}) => <h1>{name}</h1>
+
+const Content = ({parts}) => {
+  console.log('parts are', parts)
+  
+  return parts.map( part => <Part key={part.id} part={part}/> )
+  
+}
+
+const Part = ({part}) => {
+  console.log('part:',part)
+  
+  return <p>{part.name} {part.exercises}</p>
+}
+
+export default App
