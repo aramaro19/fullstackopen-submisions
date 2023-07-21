d9c171941e34b32c9f7c24337b9d0d5e91b121fd
strebor19 <arobertamaro@gmail.com>
2023-07-21 17:05:43 -0400

    do 2.5 courseinfo, separated module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,3 +1,5 @@
+import Course from './Course'
+
 const App = () => {
   const courses = [
     {
@@ -46,35 +48,10 @@ const App = () => {
 
   return <div>
     <h1>Web Development Curriculum</h1>
-    {courses.map((course) => <Course course={course}/>)}
+    {courses.map((course) => <Course key={course.id} course={course}/>)}
   </div>
 }
 
-const Course = ({course}) => {
-  return <div>
-    <Header name={course.name}/>
-    <Content parts={course.parts}/>
-    <Total parts={course.parts}/>
-  </div> 
-}
 
-const Header = ({name}) => <h2>{name}</h2>
-
-const Content = ({parts}) => {
-  console.log('parts are', parts)
-  return parts.map( part => <Part key={part.id} part={part}/> )
-}
-
-const Part = ({part}) => {
-  console.log('part:',part)
-  return <p>{part.name} {part.exercises}</p>
-}
-
-const Total = ({parts}) => {
-  const exercises = parts.map((part) => part.exercises)
-  const total = exercises.reduce((a, b) => a + b)
-  return <h4>total of {total} exercises</h4>
-  
-}
 
 export default App
