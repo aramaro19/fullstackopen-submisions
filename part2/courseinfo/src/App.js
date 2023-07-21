2d7a613d7d350724c8c993952241b534b5f96c70
strebor19 <arobertamaro@gmail.com>
2023-07-21 16:31:31 -0400

    do 2.2 courseinfo, step 7

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -28,6 +28,7 @@ const Course = ({course}) => {
   return <div>
     <Header name={course.name}/>
     <Content parts={course.parts}/>
+    <Total parts={course.parts}/>
   </div> 
 }
 
@@ -35,15 +36,19 @@ const Header = ({name}) => <h1>{name}</h1>
 
 const Content = ({parts}) => {
   console.log('parts are', parts)
-  
   return parts.map( part => <Part key={part.id} part={part}/> )
-  
 }
 
 const Part = ({part}) => {
   console.log('part:',part)
-  
   return <p>{part.name} {part.exercises}</p>
+}
+
+const Total = ({parts}) => {
+  const exercises = parts.map((part) => part.exercises)
+  const total = exercises.reduce((a, b) => a + b)
+  return <h4>total of {total} exercises</h4>
+  
 }
 
 export default App
